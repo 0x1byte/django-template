55fbd5d35c74e59da176fb91abd9715f0f6d0bea
0x1byte <mahdikh6@gmail.com>
2024-06-02 22:58:25 +0330

    fix celery remove

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,10 +21,6 @@ def remove_open_source_files():
 def remove_celery_files():
     file_names = [
         os.path.join("config", "celery_app.py"),
-        os.path.join("{{ cookiecutter.project_slug }}", "users", "tasks.py"),
-        os.path.join(
-            "{{ cookiecutter.project_slug }}", "users", "tests", "test_tasks.py"
-        ),
     ]
     for file_name in file_names:
         os.remove(file_name)
