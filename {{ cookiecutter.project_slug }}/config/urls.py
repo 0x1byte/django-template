f0dfe0e191ab7c126325c5d9a7acd4a456c3eb9c
0x1byte <mahdikh6@gmail.com>
2024-06-02 23:48:44 +0330

    fix

--- {{ cookiecutter.project_slug }}/config/urls.py
+++ {{ cookiecutter.project_slug }}/config/urls.py
@@ -17,8 +17,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     # Django Admin, use {% raw %}{% url 'admin:index' %}{% endraw %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("{{ cookiecutter.project_slug }}.users.urls", namespace="users")),
     # Your stuff: custom urls includes go here
     # ...
     # Media files
