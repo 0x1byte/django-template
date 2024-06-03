13cd0bdb52464fbeb88c0e8725c172ffc7c03cff
0x1byte <mahdikh6@gmail.com>
2024-06-03 23:49:28 +0330

    fix

--- {{ cookiecutter.project_slug }}/celery_beat_entrypoint.sh
+++ {{ cookiecutter.project_slug }}/celery_beat_entrypoint.sh
@@ -4,4 +4,4 @@ echo "--> Waiting for db to be ready"
 ./wait-for-it.sh db:5432
 
 echo "--> Starting beats process"
-poetry run celery -A config.celery_app beat -l INFO
+poetry run celery -A config.celery_app -b "${CELERY_BROKER_URL}" beat -l INFO
